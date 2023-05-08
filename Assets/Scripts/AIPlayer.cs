c38dbed503b15c12154ceeb68d57c973fb5da918
Sandra-atef25 <85317706+Sandra-atef25@users.noreply.github.com>
2023-05-08 13:32:35 +0300

    Initializing AI Class

---
+++ Assets/Scripts/AIPlayer.cs
@@ -0,0 +1,21 @@
+//AI Player Class to implement the Ai player with alpha-beta prunning algorithm
+
+using System.Collections.Generic;
+//using System.Threading;
+//using System.Threading.Tasks;
+using System.Linq;
+using System;
+
+public class AIPlayer
+{
+    private int searchDepth { set; get; }
+    private ObjectPool<GameState> gameStatePool;
+
+    public AIPlayer(int depth)
+    {
+        this.searchDepth = depth;
+        this.gameStatePool = new ObjectPool<GameState>(() => new GameState());
+    }
+
+
+}
