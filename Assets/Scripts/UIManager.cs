76e40459dfbfb291f3178d953cbb1019110ded86
Yousef497 <yousef.shalaby70@gmail.com>
2023-05-09 00:06:48 +0300

    Control the return to Main menu screen and implemented functions to control the end of the game screen with play again button

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -14,10 +14,25 @@ public class UIManager : MonoBehaviour
     private TextMeshProUGUI topText;
 
     [SerializeField]
+    private TextMeshProUGUI blackScoreText;
+
+    [SerializeField]
+    private TextMeshProUGUI whiteScoreText;
+
+    [SerializeField]
     private TextMeshProUGUI blackScoreAllGame;
 
     [SerializeField]
     private TextMeshProUGUI whiteScoreAllGame;
+
+    [SerializeField]
+    private TextMeshProUGUI winnerText;
+
+    [SerializeField]
+    private Image blackOverlay;
+
+    [SerializeField]
+    private RectTransform playAgainButton;
 
     //--------Some GameObject variables to control the scene-------
     [SerializeField]
@@ -64,16 +79,85 @@ public class UIManager : MonoBehaviour
         topText.text = message;
     }
 
+    public void SetBlackScoreText(int score)
+    {
+        blackScoreText.text = $"<sprite name=DiscBlackUp> {score}";
+    }
+
     public void SetBlackScoreAllText(int score)
     {
         blackScoreAllGame.text = $"<sprite name=DiscBlackUp> {score}";
         blackScoreAllGame.transform.LeanScale(Vector3.one * 1.2f, 0.2f).setLoopPingPong(1);
     }
 
+    public void SetWhiteScoreText(int score)
+    {
+        whiteScoreText.text = $"<sprite name=DiscWhiteUp> {score}";
+    }
+
     public void SetWhiteScoreAllText(int score)
     {
         whiteScoreAllGame.text = $"<sprite name=DiscWhiteUp> {score}";
         whiteScoreAllGame.transform.LeanScale(Vector3.one * 1.2f, 0.2f).setLoopPingPong(1);
+    }
+
+    private IEnumerator ShowOverlay()
+    {
+        blackOverlay.gameObject.SetActive(true);
+        blackOverlay.color = Color.clear;
+        blackOverlay.rectTransform.LeanAlpha(0.8f, 1);
+        yield return new WaitForSeconds(1);
+    }
+
+    private IEnumerator HideOverlay()
+    {
+        blackOverlay.rectTransform.LeanAlpha(0, 1);
+        yield return new WaitForSeconds(1);
+        blackOverlay.gameObject.SetActive(false);
+    }
+
+    private IEnumerator MoveScoreDown()
+    {
+        blackScoreText.rectTransform.LeanMoveY(0, 0.5f);
+        whiteScoreText.rectTransform.LeanMoveY(0, 0.5f);
+        yield return new WaitForSeconds(0.5f);
+    }
+
+    public void SetWinnerText(Player winner)
+    {
+        switch (winner)
+        {
+            case Player.Black:
+                winnerText.text = "Black Won!";
+                break;
+
+            case Player.White:
+                winnerText.text = "White Won!";
+                break;
+
+            case Player.None:
+                winnerText.text = "It's a Tie!";
+                break;
+        }
+    }
+
+    public IEnumerator ShowEndScreen()
+    {
+        yield return ShowOverlay();
+        yield return MoveScoreDown();
+        yield return ScaleUp(winnerText.rectTransform);
+        yield return ScaleUp(playAgainButton);
+    }
+
+    public IEnumerator HideEndScreen()
+    {
+        StartCoroutine(ScaleDown(winnerText.rectTransform));
+        StartCoroutine(ScaleDown(blackScoreText.rectTransform));
+        StartCoroutine(ScaleDown(whiteScoreText.rectTransform));
+        StartCoroutine(ScaleDown(playAgainButton));
+
+        yield return new WaitForSeconds(0.5f);
+        yield return HideOverlay();
     }
 
     //-----------------Restart and Main Menu--------------------------
