7fc73e69fb40ddc4da616271f16977aa48626b8d
salma122110 <66427066+salma122110@users.noreply.github.com>
2023-05-08 15:06:04 +0300

    Added Coin Prity Heuristic

--- Assets/Scripts/AIPlayer.cs
+++ Assets/Scripts/AIPlayer.cs
@@ -18,4 +18,55 @@ public class AIPlayer
     }
 
 
+    
+
+    //---------------------All Heuristics Evaluation Function-----------------------
+
+    private int CalculateHeuristics(GameState gameState)
+    {
+        int coinParityHeuristic = CoinParityHeuristic(gameState);
+        int actualMobilityHeuristic = ActualMobilityHeuristic(gameState);
+        int potentialMobilityHeuristic = PotentialMobilityHeuristic(gameState);
+        int cornersHeuristic = CornersCapturedHeuristic(gameState);
+
+        return (coinParityHeuristic + actualMobilityHeuristic + potentialMobilityHeuristic + cornersHeuristic);
+
+    }
+
+
+    //-------------------------Heuristics Calculations------------------------------
+
+    // Heuristics to take in consideration
+    // 1. Coin Parity
+    // 2. Mobility
+    // 3. Corners Captured
+
+    //-----------------------------Coin Parity---------------------------------------
+
+    private int CoinParityHeuristic(GameState gameState)
+    {
+        Player currentPLayer = gameState.CurrentPlayer.Opponent();
+
+        // A simple evaluation function that counts the coin parity heuristic
+        int blackScore = gameState.DiscCount[Player.Black];
+        int whiteScore = gameState.DiscCount[Player.White];
+
+        if ((blackScore + whiteScore) == 0)
+        {
+            return 0;
+        }
+
+        if (currentPLayer == Player.Black)
+        {
+            return (100 * (blackScore - whiteScore)) / (blackScore + whiteScore);
+        }
+
+        else if (currentPLayer == Player.White)
+        {
+            return (100 * (whiteScore - blackScore)) / (blackScore + whiteScore);
+        }
+
+        return 0;
+    }
+
 }
