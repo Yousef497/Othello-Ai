bd200ae1ee9e471355737fb0a1b4453ee24c9d31
Mark-GeorgeK <markgeorgekamel@outlook.com>
2023-05-11 22:49:31 +0200

    Get legal moves logic

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -65,4 +65,99 @@ public class GameState
         LegalMoves = FindLegalMoves(CurrentPlayer);
     }
 
+    
+    //---------------------------------------------------------------------
+    // to get legal moves to position the disc
+    // the position should be:
+    //  1. Empty
+    //  2. at least one disc of opponents discs outflanked
+
+    private bool IsInsideBoard(int r, int c)
+    {
+        return r >= 0 && r < Rows && c >= 0 && c < Cols;
+    }
+
+    // method of checking and storing the positions that will be outflnaked or not
+    private List<Position> OutflankedInDir(Position pos, Player player, int rDelta, int cDelta)
+    {
+        List<Position> outflanked = new List<Position>();
+        int r = pos.Row + rDelta;
+        int c = pos.Col + cDelta;
+
+        while (IsInsideBoard(r,c) && Board[r,c] != Player.None)
+        {
+            if (Board[r,c] == player.Opponent())
+            {
+                outflanked.Add(new Position(r, c));
+                r += rDelta;
+                c += cDelta;
+            }
+
+            else if (Board[r,c] == player)
+            {
+                return outflanked;
+            }
+        }
+
+        return new List<Position>();
+    }
+
+
+    // find outflanked discs in all directions
+    private List<Position> Outflanked(Position pos, Player player)
+    {
+        List<Position> outflanked = new List<Position>();
+
+        for (int rDelta = -1; rDelta <= 1; rDelta++)
+        {
+            for (int cDelta = -1; cDelta <= 1; cDelta++)
+            {
+                if (rDelta == 0 && cDelta == 0)
+                {
+                    continue;
+                }
+
+                outflanked.AddRange(OutflankedInDir(pos, player, rDelta, cDelta));
+            }
+        }
+
+        return outflanked;
+
+    }
+
+
+    private bool IsMoveLegal(Player player, Position pos, out List<Position> outflanked)
+    {
+        if (Board[pos.Row, pos.Col] != Player.None)
+        {
+            outflanked = null;
+            return false;
+        }
+
+        outflanked = Outflanked(pos, player);
+        return outflanked.Count > 0;
+    }
+
+
+    public Dictionary<Position, List<Position>> FindLegalMoves(Player player)
+    {
+        Dictionary<Position, List<Position>> legalMoves = new Dictionary<Position, List<Position>>();
+
+        for (int r = 0; r < Rows; r++)
+        {
+            for (int c = 0; c < Cols; c++)
+            {
+                Position pos = new Position(r, c);
+
+                if (IsMoveLegal(player, pos, out List<Position> outflanked))
+                {
+                    legalMoves[pos] = outflanked;
+                }
+            }
+        }
+
+        //Debug.Log(legalMoves.Count);
+        return legalMoves;
+    }
+
 }
