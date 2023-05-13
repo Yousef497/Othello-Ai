764f03113cf76738ef61cc9e124125a68b3cc36f
MariaOsama2 <1809094@eng.asu.edu.eg>
2023-05-13 16:03:17 +0300

    Helper methods

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -35,3 +35,52 @@ public class GameManager : MonoBehaviour
     private GameState gameState = new GameState();
     private Disc[,] discs = new Disc[8, 8];
     private List<GameObject> highlights = new List<GameObject>();
+
+
+    //------------------------------------------------------------
+
+
+    // ----------------------------Functions and Methods--------------------------------
+
+
+    // --------------------Some Helper Methods---------------------------
+
+    private Position SceneToBoardPos(Vector3 scenePos)
+    {
+        int col = (int)(scenePos.x - 0.25f);
+        int row = 7 - (int)(scenePos.z - 0.25f);
+        return new Position(row, col);
+    }
+
+    private Vector3 BoardToScenePos(Position boardPos)
+    {
+        return new Vector3(boardPos.Col + 0.75f, 0, 7 - boardPos.Row + 0.75f);
+    }
+
+    private void SpawnDisc(Disc prefab, Position boardPos)
+    {
+        Vector3 scenePos = BoardToScenePos(boardPos) + Vector3.up * 0.1f;
+        discs[boardPos.Row, boardPos.Col] = Instantiate(prefab, scenePos, Quaternion.identity);
+        //Debug.Log(scenePos);
+    }
+
+    private void AddStartDiscs()
+    {
+        foreach (Position boardPos in gameState.OccupiedPositions())
+        {
+            Player player = gameState.Board[boardPos.Row, boardPos.Col];
+            SpawnDisc(discPrefabs[player], boardPos);
+        }
+
+        //Debug.Log(gameState.OccupiedPositions());
+    }
+
+    private void FlipDiscs(List<Position> positions)
+    {
+        foreach (Position boardPos in positions)
+        {
+            discs[boardPos.Row, boardPos.Col].Flip();
+        }
+    }
+
+}
